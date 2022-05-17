2eb5c6ca66a5432c4cd0220ebb8f371c7fc5d932
Wolthrax <wolthrax@gmail.com>
2022-05-17 08:39:28 +0300

    MOZ-358 Added left join to GET_NODE_ADDRESS_BY_TIME_INTERVAL_PARAMETERS

--- src/main/java/com/mapofzones/endpointchecker/common/constants/QueryConstants.java
+++ src/main/java/com/mapofzones/endpointchecker/common/constants/QueryConstants.java
@@ -6,8 +6,8 @@ public interface QueryConstants {
 
         String GET_NODE_ADDRESS_BY_TIME_INTERVAL_PARAMETERS =
                 "SELECT * FROM nodes_addrs zn " +
-                "JOIN nodes_lcd_addrs lcd on zn.ip_or_dns = lcd.ip_or_dns " +
-                "JOIN nodes_rpc_addrs rpc on zn.ip_or_dns = rpc.ip_or_dns " +
+                "LEFT JOIN nodes_lcd_addrs lcd on zn.ip_or_dns = lcd.ip_or_dns " +
+                "LEFT JOIN nodes_rpc_addrs rpc on zn.ip_or_dns = rpc.ip_or_dns " +
                 "WHERE lcd.last_active < :from AND lcd.last_active > :to AND lcd.last_checked_at < :timeToCheck " +
                 "OR rpc.last_active < :from AND rpc.last_active > :to AND rpc.last_checked_at < :timeToCheck " +
                 "ORDER BY zn.last_checked_at ASC LIMIT :limit";
